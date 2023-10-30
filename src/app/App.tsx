8bc4ff161d59359fc769fad0fae3e1a9edfd1cf4
ba888ton <38305528+Ba888Ton@users.noreply.github.com>
2023-10-30 19:37:48 +0300

    FEAT add navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,16 @@
-import { Link } from "react-router-dom"
 import { useTheme } from "../app/providers/ThemeProvider"
 
 import { classNames } from "../helpers/classNames/classNames"
 import './styles/index.scss'
 import { AppRouter } from "./providers/router"
+import { Navbar } from "../wigets/Navbar"
 
 export default function App() {
   const {theme, toggleTheme} = useTheme();
 
   return (
     <div className={classNames('app', {}, [theme])}>
-      <Link to={'/'}>на главную</Link>
-      <Link to={'/about'}>в описание</Link>
+      <Navbar />
       <button onClick={toggleTheme}>переключить тему</button>
       <AppRouter />
     </div>
---
+++ src/wigets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,19 @@
+import { classNames } from "../../../helpers/classNames/classNames";
+import cls from "./Navbar.module.scss"
+import { AppLink, AppLinkTheme } from "../../../shared/ui/AppLink/AppLink";
+
+interface NavbarProps {
+  className?: string;
+}
+
+export function Navbar({className}: NavbarProps) {
+  return (
+    <div className={classNames(cls.Navbar, {}, [className])}>
+      <div className={cls.links}>
+        <AppLink theme={AppLinkTheme.SECONDARY} to={'/'}>на главную</AppLink>
+        <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>в описание</AppLink>
+      </div>
+    </div>
+  )
+}
+
